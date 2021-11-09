cd8f41c778fac79b68cf3c23b5d851f619603226
EnderdracheLP <enderdrachelp@gmail.com>
2021-11-09 21:01:27 +0100

    Fix Lag Reducer Toggle overwriting Zen Mode modifier

--- MultiQuestensions/src/Hooks/InstallerHooks.cpp
+++ MultiQuestensions/src/Hooks/InstallerHooks.cpp
@@ -13,10 +13,10 @@ using namespace GlobalNamespace;
 namespace MultiQuestensions {
     MAKE_HOOK_MATCH(MultiplayerConnectedPlayerInstaller_InstallBindings, &MultiplayerConnectedPlayerInstaller::InstallBindings, void, MultiplayerConnectedPlayerInstaller* self) {
         getLogger().debug("LagReducer set to '%s'", getConfig().config["LagReducer"].GetBool() ? "true" : "false");
-        self->dyn__sceneSetupData()->gameplayModifiers->dyn__zenMode() = getConfig().config["LagReducer"].GetBool();
-        getLogger().debug("[Prefix] ZenMode is set to '%s'", self->dyn__sceneSetupData()->gameplayModifiers->dyn__zenMode() ? "true" : "false");
+        self->dyn__sceneSetupData()->dyn_gameplayModifiers()->dyn__zenMode() |= getConfig().config["LagReducer"].GetBool();
+        getLogger().debug("[Prefix] ZenMode is set to '%s'", self->dyn__sceneSetupData()->dyn_gameplayModifiers()->dyn__zenMode() ? "true" : "false");
         MultiplayerConnectedPlayerInstaller_InstallBindings(self);
-        getLogger().debug("[Postfix] ZenMode is set to '%s'", self->dyn__sceneSetupData()->gameplayModifiers->dyn__zenMode() ? "true" : "false");
+        getLogger().debug("[Postfix] ZenMode is set to '%s'", self->dyn__sceneSetupData()->dyn_gameplayModifiers()->dyn__zenMode() ? "true" : "false");
     }
 
     void Hooks::InstallerHooks() {
