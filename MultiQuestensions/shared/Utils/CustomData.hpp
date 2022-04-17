7cb6796693c5c1b66406148d4342cc0dd2ec27d8
EnderdracheLP <enderdrachelp@gmail.com>
2022-04-17 15:39:19 +0200

    Remove unused files

--- MultiQuestensions/shared/Utils/CustomData.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <string>
-#include "GlobalNamespace/CustomPreviewBeatmapLevel.hpp"
-
-namespace MultiQuestensions::Utils {
-    bool HasRequirement(const std::optional<GlobalNamespace::CustomPreviewBeatmapLevel*>& beatmapOpt);
-}
