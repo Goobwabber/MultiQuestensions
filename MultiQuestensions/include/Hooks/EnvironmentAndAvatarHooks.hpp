268b448ecefc7fb323b06f9969b5a574a74c39fe
EnderdracheLP <enderdrachelp@gmail.com>
2022-04-22 00:23:14 +0200

    Add extern _multiplayerSessionManager

--- MultiQuestensions/include/Hooks/EnvironmentAndAvatarHooks.hpp
+++ MultiQuestensions/include/Hooks/EnvironmentAndAvatarHooks.hpp
@@ -4,6 +4,12 @@
 
 namespace MultiQuestensions {
     extern void SetPlayerPlaceColor(GlobalNamespace::IConnectedPlayer* player, const UnityEngine::Color& color, bool priority);
+    extern void UpdateNameTagIcons();
     // extern void CreateOrUpdateNameTag(GlobalNamespace::IConnectedPlayer* player);
     // extern void SetPlatformData(MultiplayerCore::Players::MpPlayerData* data);
+}
+
+#include "GlobalNamespace/MultiplayerSessionManager.hpp"
+namespace MultiplayerCore {
+    extern GlobalNamespace::MultiplayerSessionManager* _multiplayerSessionManager;
 }
